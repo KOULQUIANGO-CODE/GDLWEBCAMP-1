62efac82b0dfd606804b7bdbf2447340c36305f0
Kevin Ulquiango <78171886+KOULQUIANGO-CODE@users.noreply.github.com>
2021-03-19 13:06:43 -0500

    PRUEBA

--- includes/funciones/bd_conexion.php
+++ includes/funciones/bd_conexion.php
@@ -1,6 +1,6 @@
 <?php
 // conexion a la base de datos gdlwebcamp
-$conn=new mysqli('b8k903tj9gxzhqdrk6j9-mysql.services.clever-cloud.com','u8scirgmoouqhfyj','mwgfHJ9Egajy0B5rqfHL','b8k903tj9gxzhqdrk6j9');
+$conn=new mysqli('123b8k903tj9gxzhqdrk6j9-mysql.services.clever-cloud.com','u8scirgmoouqhfyj','mwgfHJ9Egajy0B5rqfHL','b8k903tj9gxzhqdrk6j9');
 
     if($conn->connect_error) {
         echo $conn->connect_error;
